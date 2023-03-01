6d3b1c16fcc7c306ea2bd8330db6500dc4945e1c
BM Phuoc <phuoc.vo@brickmate.kr>
2023-03-01 15:01:58 +0700

    section 4: Database and TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TasksModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 33060,
+      username: 'root',
+      password: '12qwaszx',
+      database: 'nest-tut',
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
 })
 export class AppModule {}
