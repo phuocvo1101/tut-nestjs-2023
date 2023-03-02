54c5c8e4f69019d9afb088586e33a420dcc4772a
Phuoc Vo <phuocvo@brickmate.kr>
2023-03-02 17:22:34 +0700

    auth part 2: to section 73

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Post, Delete, Patch } from '@nestjs/common';
 import { Body, Param, Query, UseGuards } from '@nestjs/common/decorators';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
@@ -25,8 +27,11 @@ export class TasksController {
     }
 
     @Post()
-    createTask( @Body() createTaskDto: CreateTaskDto ): Promise<Task> {
-        return this.tasksService.createTask(createTaskDto);
+    createTask( 
+        @Body() createTaskDto: CreateTaskDto,
+        @GetUser() user: User
+        ): Promise<Task> {
+        return this.tasksService.createTask(createTaskDto, user);
     }
 
     @Delete('/:id')
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -6,6 +6,7 @@ import { TaskRepository } from './tasks.repository';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 import { Repository } from 'typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class TasksService {
@@ -28,8 +29,8 @@ export class TasksService {
         
     }
 
-    createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-        return this.tasksRepository.createTask(createTaskDto);
+    createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
+        return this.tasksRepository.createTask(createTaskDto, user);
     }
 
     async deleteTask(id: string): Promise<void> {
